c38cc8344b45b024f103cafc5d2fe4dc081e91aa
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-04-13 07:45:47 +0500

    без миксинов

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
+import { defineConfig } from 'vite';
 import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 
-export default () => {
-  return {
-      plugins: [
-          createSvgSpritePlugin({
-              exportType: 'vanilla',
-              include: '**/icons/*.svg'
-          }),
-      ],
-  }
-};
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '@scss/global/variables' as *;
+          @use '@scss/global/mixins' as *;
+        `,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@scss': '/src/scss'
+    }
+  },
+  plugins: [
+    createSvgSpritePlugin({
+      exportType: 'vanilla',
+      include: '**/icons/*.svg'
+    }),
+  ],
+});
