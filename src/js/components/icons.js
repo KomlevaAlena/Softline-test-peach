071ccd34423c41f22e11923c39d0e297129b1f5e
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-04-13 15:02:07 +0500

    svg

--- src/js/components/icons.js
+++ src/js/components/icons.js
@@ -1,11 +1,15 @@
 import BirthdayIcon from '../../icons/birthday.svg';
 import LogoIcon from '../../icons/soft-line-logo.svg';
 import FilterArrow from '../../icons/filter-arrow.svg';
+import Dms from '../../icons/dms.svg';
+import Phone from '../../icons/phone.svg';
 
 const Icons = {
     birthday: BirthdayIcon,
     logoIcon: LogoIcon,
-    filterArrow: FilterArrow
+    filterArrow: FilterArrow,
+    dms: Dms,
+    phone: Phone
 };
 
 document.querySelectorAll('[iconName]').forEach(iconElement => {
