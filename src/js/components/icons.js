a76becbd8886d85aa87a067ce92f80ad17b9d361
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-04-14 12:01:37 +0500

    icons corp

--- src/js/components/icons.js
+++ src/js/components/icons.js
@@ -3,13 +3,21 @@ import LogoIcon from '../../icons/soft-line-logo.svg';
 import FilterArrow from '../../icons/filter-arrow.svg';
 import Dms from '../../icons/dms.svg';
 import Phone from '../../icons/phone.svg';
+import Sport from '../../icons/sport.svg';
+import Lti from '../../icons/lti.svg';
+import Technique from '../../icons/technique.svg';
+import Home from '../../icons/home.svg';
 
 const Icons = {
     birthday: BirthdayIcon,
     logoIcon: LogoIcon,
     filterArrow: FilterArrow,
     dms: Dms,
-    phone: Phone
+    phone: Phone,
+    sport: Sport,
+    lti: Lti,
+    technique: Technique,
+    home: Home
 };
 
 document.querySelectorAll('[iconName]').forEach(iconElement => {
